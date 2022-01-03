e5f5b7991fbdaf6636bcb091a78828f1b6d7be17
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-01-03 16:57:41 -0300

    First commit

---
+++ src/main/java/com/example/demo/entities/User.java
@@ -0,0 +1,80 @@
+package com.example.demo.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class User implements Serializable {
+    public static final long serialVersionUID =1L;
+
+    private Long id;
+    private String Name;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, String password) {
+        this.id = id;
+        Name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
---
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.example.demo.resources;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L,"maria","maria@gmail","99999","12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
