7a0a2a202df5a0f0c65c42d157eca1f41fa51a11
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-01-04 09:38:04 -0300

    h2 e jpa commit

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -1,12 +1,18 @@
 package com.example.demo.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
-
+@Entity
 public class User implements Serializable {
     public static final long serialVersionUID =1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String Name;
     private String email;
