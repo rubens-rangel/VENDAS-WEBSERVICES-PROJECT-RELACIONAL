b009ad71e898286a75dfb5a3327e3f85ba930ded
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-01-11 09:56:26 -0300

    Subtotal and total methods implementation

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -92,6 +92,14 @@ public class Order implements Serializable {
         this.client = client;
     }
 
+    public Double getTotal(){
+        double sum = 0.0;
+        for(OrderItem x : items){
+            sum= sum + x.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/main/java/com/example/demo/entities/Payment.java
+++ src/main/java/com/example/demo/entities/Payment.java
@@ -1,6 +1,7 @@
 package com.example.demo.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -21,6 +22,7 @@ public class Payment implements Serializable {
 
     @OneToOne
     @MapsId
+    @JsonIgnore
     private Order order;
 
     public Payment() {
