9e44e7c98b14a43e370cf5d26216ee5226e55fb5
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-01-11 13:41:03 -0300

    update user implementation

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -41,4 +41,9 @@ public class UserResource {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        obj= service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
 }
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -14,19 +14,32 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repository.findAll();
     }
-    public User findById(Long id){
+
+    public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
         return obj.get();
     }
 
-    public User insert(User obj){
+    public User insert(User obj) {
         return repository.save(obj);
     }
 
-    public void  delete(Long id){
+    public void delete(Long id) {
         repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getOne(id);
+        updateData(entity,obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
