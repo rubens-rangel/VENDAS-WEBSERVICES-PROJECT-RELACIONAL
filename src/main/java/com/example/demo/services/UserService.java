d361e7322ecceddd4fdc16cbed1a8f0ccb8c1a78
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-01-12 10:04:51 -0300

    exception update implementation

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
+        try{
         User entity = repository.getOne(id);
         updateData(entity,obj);
-        return repository.save(entity);
+        return repository.save(entity);}
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
